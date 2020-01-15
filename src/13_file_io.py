57fabcccb3ec25205a0bef423a8744ff71fb9663
Israel Gonzalez <48233351+israelgonzalezb@users.noreply.github.com>
2020-01-15 11:51:44 -0800

    Completed file io challenge

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,6 +9,13 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+import os
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(THIS_FOLDER, "foo.txt")
+file = open(file_path,"r")
+
+print(file.read())
+file.close()
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -16,3 +23,11 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+
+file_path = os.path.join(THIS_FOLDER, "bar.txt")
+file = open(file_path,"w")
+file.write("hello\nworld\n!!!!!")
+file = open(file_path,"r")
+
+print(file.read())
+file.close()
